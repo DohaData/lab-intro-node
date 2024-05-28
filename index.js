18a866193b1281bb1b5aa5e32936d2e0f39a373f
Doha Kaddaf <doha.kaddaf@gmail.com>
2024-05-28 21:15:40 +0200

    finish lab

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let index = this.items.findIndex((e) => e >= item);
+    this.items.splice(index === -1 ? this.length : index, 0, item);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc, ele) => acc + ele, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (!this.length) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items.reduce((acc, ele) => acc + ele, 0) / this.length;
+  }
 }
 
 module.exports = SortedList;
